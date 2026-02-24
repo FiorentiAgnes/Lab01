5bf7b217e80817cbd4c84a2f12bf097bb211c71f
Agnese <agnesefioreneti@gmail.com>
2026-02-24 14:36:59 +0100

    Prima modifica

---
+++ Lab01/main.py
@@ -0,0 +1,7 @@
+class Domanda:
+    def __init__(self, testo, livello, rispCorretta, rispErrata1, rispErrata2):
+        self.testo = testo
+        self.livello = livello
+        self.rispCorretta = rispCorretta
+        self.rispErrata1 = rispErrata1
+        self.rispErrata2= rispErrata2
